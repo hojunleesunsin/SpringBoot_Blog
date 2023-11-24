b78b5643cc4ff8f74e62188abae95812bc46010b
hojunlee <hongpc0099@gmail.com>
2023-11-24 14:52:39 +0900

    update 블로그 글 조회 관련 articleList.html, BlogViewController.java

--- src/main/java/me/leehojun/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/leehojun/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.leehojun.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.leehojun.springbootdeveloper.domain.Article;
 import me.leehojun.springbootdeveloper.dto.ArticleListViewResponse;
+import me.leehojun.springbootdeveloper.dto.ArticleViewResponse;
 import me.leehojun.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -24,5 +27,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
