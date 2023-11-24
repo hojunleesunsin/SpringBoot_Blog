b9103ea3e0fe6f7515915d13fc8e13e2faa45f36
hojunlee <hongpc0099@gmail.com>
2023-11-24 14:53:44 +0900

    update 블로그 글 생성, 업데이트 시간 code 추가

--- src/main/java/me/leehojun/springbootdeveloper/domain/Article.java
+++ src/main/java/me/leehojun/springbootdeveloper/domain/Article.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -32,4 +36,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
